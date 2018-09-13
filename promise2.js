a15fd4849d282005eab43cd0093c04907cf69583
Mark Newman's Temp Account <mwntemp@Marks-MacBook-Pro.local>
2018-09-13 12:10:46 -0400

    First commit

---
+++ promise2.js
@@ -0,0 +1,39 @@
+///// Problem 1 - Fun with Promises
+
+function flip() {
+    let p = new Promise(function (resolve, reject) {
+        if (Math.random() > 0.5) resolve();
+        else reject();
+    });
+    return p;
+}
+
+//// Add code here that will "flip" the coin ten times and write the 
+//// result to the console (e.g. "Heads" or "Tails" for each flip).
+for (let i = 0; i < 10; i++) {
+    flip().then(() => console.log("Heads!"), () => console.log("Tails!"));
+}
+
+
+
+///// Problem 2 - More fun...
+
+function countBig(bignum) {
+    // Add code here that returns a Promise that will resolve after it has counted to bignum
+
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < bignum; i++) {
+            // just counting here...
+        }
+        resolve();
+    })
+}
+
+
+start = Date.now();
+bignum = 1000000000;
+countBig(1000000000).then(()=> {
+    console.log("It took " + (Date.now() - start) + " ms to count to " + bignum);
+}, () => {
+    console.log("A problem occurred while trying to count to " + bignum);
+})
